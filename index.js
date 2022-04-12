e7e2f459d5a398fd66552165b0ec05eafabaf0fd
FaresHadiji <fares.hadiji@gmail.com>
2022-04-12 14:30:51 +0200

    Premier test brain

--- index.js
+++ index.js
@@ -1,22 +1,43 @@
-const Discord = require('discord.js');
-const client = new Discord.Client({
-   intents: [
-      Discord.Intents.FLAGS.GUILDS,
-      Discord.Intents.FLAGS.GUILD_MESSAGES
-   ]
-});
+const RiveScript= require("rivescript")
+const { Client, Intents } = require("discord.js")
+const bot = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] })
 
 const {token} = require('./config.json');
 
+var chloe = new RiveScript();
 
-client.once('ready', () => {
-   console.log('Félicitations, votre bot Discord a été correctement initialisé !');
-});
+chloe.loadDirectory("brain").then(loading_done).catch(loading_error)
 
-client.on("message", message => {
-   if (message.content === "!ping") {
-     message.channel.send("Pong.")
-   }
- })
+function loading_done() {
+  console.log("Chloé a fini d'apprendre ");
+  chloe.sortReplies();
+}
 
-client.login(token);
+
+function loading_error(error, filename, lineno) {
+  console.log("Error when loading files: " + error);
+}
+
+bot.on('ready', function () {console.log("Je suis connectée !")})
+
+bot.on('messageCreate', message => {
+		if(message.channel.name == "general" && message.author.id != bot.application.id)
+		{
+			let entry = message.content 
+			chloe.reply(message.author.name, entry).then(function(reply)
+				{
+					var output = reply;
+					if(output != "ERR: No Reply Matched")
+					{
+						message.channel.send(output)
+					}
+					else
+					{
+						message.channel.send("Exprime toi mieux")
+					}
+				}
+			);
+		}
+	}
+)
+bot.login(token)
