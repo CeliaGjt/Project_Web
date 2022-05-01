41348d03e3698355863b853b2fdd8e71ffb4af53
FaresHadiji <fares.hadiji@gmail.com>
2022-05-01 02:16:18 +0200

    page html marche + app.get(/... marche

--- server/model/Bot.mjs
+++ server/model/Bot.mjs
@@ -1,79 +1,77 @@
 class Bot{
-    static id = this.id;
-    static name = this.name;
-    static cerveau = this.cerveau;
 
+  static id = this.id;
+  static name = this.name;
+  static cerveau  = this.cerveau;
 
-    constructor(data){
-        if(undefined != data.id) { 
-            if(!isInt(data.id)){
-            throw new Error("Bot Creation : passed Id is not an integer");
-            }
-            this.id = data.id;
-        } else {
-            this.id = parseInt(Math.floor(Math.random() * Math.floor(100000))     );
-        }
-
-        if(undefined != data.name) {
-            if(!isString(data.name)){
-            throw new Error("Bot Creation : passed Title is not a string");
-            }
-            this.name = data.name;
-        } else {
-            this.name = "";
-        }
-
-        if(undefined != data.cerveau) {
-            if(!isString(data.cerveau)){
-              throw new Error("Bot Creation : passed cerveau is not a cerveau");
-            }
-            this.cerveau = data.cerveau;
-          } else {
-            this.cerveau = "";
-          }
+  constructor(data){   //id,title,comment,tags
+    if(undefined != data.id) {
+      if(!isInt(data.id)){
+        throw new Error("Bot Creation : passed Id is not an integer");
+      }
+      this.id = data.id;
+    } else {
+      this.id = parseInt(    Math.floor(Math.random() * Math.floor(100000))     );
+    }
+    if(undefined != data.name) {
+      if(!isString(data.name)){
+        throw new Error("Bot Creation : passed Title is not a string");
+      }
+      this.name = data.name;
+    } else {
+      this.name = "";
+    }
+    if(undefined != data.cerveau) {
+      if(!isString(data.cerveau)){
+        throw new Error("Bot Creation : passed cerveau is not a cerveau");
+      }
+      this.cerveau = data.cerveau;
+    } else {
+      this.cerveau = "";
     }
 
-    static async create(id,name, cerveau){ 
-        return new Bot(id,name, cerveau);
-    }
+  }
+  
+  static async create(name, cerveau){ 
+    return new Bot(name, cerveau);
+  }
+  static isBot(anObject){
+    // check if mandatory fields are there
+    let hasMandatoryProperties = Object.keys(this).every(key=> anObject.hasOwnProperty(key));
+    // we should also check the property values (if are strings, etc ... as in constructor) 
+    return hasMandatoryProperties;
+  }
 
-    static isBot(anObject){
-        // check if mandatory fields are there
-        let hasMandatoryProperties = Object.keys(this).every(key=> anObject.hasOwnProperty(key));
-        // we should also check the property values (if are strings, etc ... as in constructor) 
-        return hasMandatoryProperties;
+  static isValidProperty(propertyName,propertyValue) {
+    if(!this.hasOwnProperty(propertyName)){
+      return false;
     }
-    
-    static isValidProperty(propertyName,propertyValue) {
-        if(!this.hasOwnProperty(propertyName)){
-          return false;
-        }
-        // we should also check the property values (if are strings, etc ... as in constructor) 
-        return true;
-    }
+    // we should also check the property values (if are strings, etc ... as in constructor) 
+    return true;
+  }
+
 }
-    
+
 function isInt(value) {
-    let x = parseFloat(value);
-    return !isNaN(value) && (x | 0) === x;
+  let x = parseFloat(value);
+  return !isNaN(value) && (x | 0) === x;
 }
 
 function isString(myVar) {
-    return (typeof myVar === 'string' || myVar instanceof String);
+  return (typeof myVar === 'string' || myVar instanceof String) ;
 }
 
-function isDate(x) {
-    return (null != x) && !isNaN(x) && ("undefined" !== typeof x.getDate);
+function isDate (x) 
+{ 
+  return (null != x) && !isNaN(x) && ("undefined" !== typeof x.getDate); 
 }
 
-function isArrayOfStrings(value) {
-    if (!Array.isArray(value)) return false;
-    for (let item of value) {
-        if (!isString(item)) return false;
-    }
-    return true;
+function isArrayOfStrings(value){
+  if(!Array.isArray(value)) return false;
+  for(let item of value){
+    if(!isString(item)) return false;
+  }
+  return true;
 }
-    
-
 
 export {Bot}
